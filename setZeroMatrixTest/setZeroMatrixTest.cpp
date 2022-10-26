bba9b4142e6838f74b4d20d855208f579e7e10d2
Artemsd1 <102757519+Artemsd1@users.noreply.github.com>
2022-10-27 01:07:07 +0300

    Add files via upload

---
+++ setZeroMatrixTest/setZeroMatrixTest.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MatrixCalculator/Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace setZeroMatrixTest
+{
+	TEST_CLASS(setZeroMatrixTest)
+	{
+	public:
+		
+		TEST_METHOD(typeTest)
+		{
+			Matrix matr;
+			setZeroMatrix(&matr);
+
+			for (int i = 0; i < matr.height; i++)
+				for (int j = 0; j < matr.width; j++)
+					Assert::AreEqual(0, matr.values[i][j]);
+		}
+	};
+}
