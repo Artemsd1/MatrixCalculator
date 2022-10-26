0c0b267232e1d4148337ef9f24a29d8f98c062ac
Artemsd1 <102757519+Artemsd1@users.noreply.github.com>
2022-10-27 01:09:46 +0300

    Add files via upload

---
+++ getValueTest/getValueTest.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MatrixCalculator/Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace getValueTest
+{
+	TEST_CLASS(getValueTest)
+	{
+	public:
+		//int getValue(char* value)
+
+		TEST_METHOD(TestMethod1)
+		{
+			string line = "2";
+			char* number = &line[0];
+			int exp = 2;
+			int res = getValue(number);
+
+			Assert::AreEqual(exp, res);
+		}
+
+		TEST_METHOD(negativeNumber)
+		{
+			string line = "-12";
+			char* number = &line[0];
+			int exp = -12;
+			int res = getValue(number);
+
+			Assert::AreEqual(exp, res);
+		}
+
+		TEST_METHOD(longNumber)
+		{
+			string line = "21312432";
+			char* number = &line[0];
+			int exp = 21312432;
+			int res = getValue(number);
+
+			Assert::AreEqual(exp, res);
+		}
+	};
+}
---
+++ isSizeCorrectTest/isSizeCorrectTest.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MatrixCalculator/Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace isSizeCorrectTest
+{
+	TEST_CLASS(isSizeCorrectTest)
+	{
+	public:
+		
+		//void isSizeCorrect(int size)
+		TEST_METHOD(typeTest)
+		{
+			int N = 14;
+			isSizeCorrect(N);
+		}
+
+		TEST_METHOD(maxSize)
+		{
+			int N = 20;
+			isSizeCorrect(N);
+		}
+
+		TEST_METHOD(minSize)
+		{
+			int N = 1;
+			isSizeCorrect(N);
+		}
+	};
+}
